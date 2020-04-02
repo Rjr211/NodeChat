b2f4c9f6748bd4ef4a2c3320f6723c53687ae449
Rob <robert.j1450@gmail.com>
2020-04-01 21:16:12 -0400

    refactored code to use socket.io, required socket.io module, loaded client side socket.io, tested server to support web sockets/success

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const  app = express()
+const server = http.createServer(app)
+//server supports websockets
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 
@@ -9,6 +14,13 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', () => {
+     console.log('New incoming connection')
+})
+
+//start http server
+server.listen(port, () => {
     console.log(`App is listening on port ${port}`)
 })
+
+//refactored code to use socket.io, required socket.io module, loaded client side socket.io, tested server
