18188e9f32f474764f0bfb8eb47b399967f73608
Rob <robert.j1450@gmail.com>
2020-04-05 22:51:51 -0400

    added user joined/left functionality in dev tools

--- src/index.js
+++ src/index.js
@@ -14,8 +14,41 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+
+// let count = 0;
+
+// server (emit) => client (recieve) - countUpdated
+//client (emit) => server (recieve) - increment
+
+
+io.on('connection', (socket) => {
      console.log('New incoming connection')
+
+     //emits to specisifc connection
+    socket.emit('message', 'Welcome!')
+
+    //emits to everybody but new connection
+    socket.broadcast.emit('message', 'A new user has joined!')
+    // //send an event on server
+    //  socket.emit('countUpdated', count)
+
+    // //listening for event on client
+    //  socket.on('increment', () => {
+    //      count++
+    //      //listens for single event
+    //      //socket.emit('countUpdated', count)
+
+    //     //listens for all event
+    //      io.emit('countUpdated', count)
+    //  })
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
 })
 
 //start http server
@@ -23,4 +56,3 @@ server.listen(port, () => {
     console.log(`App is listening on port ${port}`)
 })
 
-//refactored code to use socket.io, required socket.io module, loaded client side socket.io, tested server
